f932e9b421981d9548f68f4d633b4c8be3ee3488
Quentin <quentin@waninbank.com>
2016-04-09 22:32:54 +0800

    no message

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,7 +31,7 @@ check_login(){
 cd /logmein-hamachi-2.1.0.139-x64
 ./install.sh
 start_daemon
-/etc/init.d/logmein-hamachi start
+su -c "/etc/init.d/logmein-hamachi start"
 #check_login
 hamachi login
 hamachi join $HAMACHI_NET_ACC $HAMACHI_NET_PASS
