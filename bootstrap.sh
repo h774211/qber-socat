7635931687a35767c0ce451f1ddd5347bca84d9f
Quentin <quentin@waninbank.com>
2016-04-09 20:49:54 +0800

    no message

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
+check_login(){
+  IFS=$'\n';
+  regex_status="status.*?: (.*?)"
+  regex_account="lmi account:(.*?)"
+  for line in $(hamachi); do
+    if [[ $line =~ $regex_status ]]; then
+      if [[ ${BASH_REMATCH[1]} == offline ]]; then
+        while [ 1 ]; do
+          out=$(hamachi login)
+          [[ $out =~ ok ]] && break || sleep 1
+        done
+      fi
+    fi
+  done
+  while [ 1 ]; do
+    out=$(hamachi)
+    [[ $out =~ "loggin in" ]] && sleep 1
+    [[ $out =~ "logged in" ]] && break
+  done
+}
+
 cd /logmein-hamachi-2.1.0.139-x64
 ./install.sh
-hamachi login
+
+check_login
 hamachi join $HAMACHI_NET_ACC $HAMACHI_NET_PASS
 socat $LOACL_HOST $REMOTE_HOST
