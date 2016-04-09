0a2cc488373dfbfed2b7775b3a8add1d103bd27a
Quentin <quentin@waninbank.com>
2016-04-09 21:33:53 +0800

    no message

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-start_daemon(){
-  /opt/logmein-hamachi/hamachid -c /config
-  while [ 1 ]; do
-    out=$(hamachi)
-    [[ $out =~ *version* ]] && sleep 1 || break
-  done
-}
-
 check_login(){
   IFS=$'\n';
   regex_status="status.*?: (.*?)"
@@ -31,7 +23,6 @@ check_login(){
 cd /logmein-hamachi-2.1.0.139-x64
 ./install.sh
 
-start_daemon
 check_login
 hamachi join $HAMACHI_NET_ACC $HAMACHI_NET_PASS
 socat $LOACL_HOST $REMOTE_HOST
